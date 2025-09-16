b9abee835e56b8a557c1e5a3f4193d65008fe6ef
Don-Nor <norri127@purdue.edu>
2025-09-15 20:30:37 -0400

    Start
    First commit of the project. Adding the rocket animation subfunction (given), and creating the main function.

---
+++ ThreeDoF_main.m
@@ -0,0 +1,17 @@
+% Author: Donovan Norris
+
+%% Program Description
+
+%% Initialization
+
+%% Parameters
+
+thrust = 2000 ; % Given thrust parameter, N
+mass = 30 ; % Given mass parameter, kg
+burnTime = 5 ; % Given burn time parameter, s
+outerDiameter = 0.1 ; % Given outer diamter of the rocket parameter, m
+dragCoefficient = 0.75 ; % Given drag coefficient paramete
+
+%% Calculations
+
+%% Display
