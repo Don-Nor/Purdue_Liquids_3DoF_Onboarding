8d1bc8f3aef43d34aa32b2d70c6a9929a44c56c6
Don-Nor <norri127@purdue.edu>
2025-09-29 22:45:45 -0400

    Height Graphing 3DOF
    Working Height Graphing 3DOF model

--- ThreeDoF_main.m
+++ ThreeDoF_main.m
@@ -3,48 +3,54 @@
 %% Program Description
 
 %% Initialization
+clear;
 clc;
 
 %% Parameters
 
 thrust = 2000 ; % Given thrust parameter, N
-mass = 30 ; % Given mass parameter, kg
-burnTime = 5 ; % Given burn time parameter, s
+mass = 40 ; % Given mass parameter, kg
+burnTime = 10 ; % Given burn time parameter, s
 outerDiameter = 0.1 ; % Given outer diamter of the rocket parameter, m
 dragCoefficient = 0.75 ; % Given drag coefficient
 g = 9.8; % Gravity m/s^2
-area = 2 * pi * (outerDiameter / 2) ^ 2; % Area of the rocket, m^2
-launchAngle = (85 / 180) * pi; % Launch angle in radians from ground up, rads
-x0 = 0; % Initial Launch Height, m
-densityAir = 1; % Air density (constant), 
-tspan = [0 5];
+area = pi * (outerDiameter / 2) ^ 2; % Area of the rocket, m^2
+phi = (5 / 180) * pi; % Launch angle in radians from top down, rads
+theta = (0 / 180) * pi; % Launch angle in radius from x-axis sweeping positive, rads
+x0 = [0, 0, 0]; % Initial Launch Height, m
+xdot0 = [0, 0, 0]; % Initial Launch Velocity, m
+densityAir = 1.225; % Air density (constant), 
+tspan = [0 70];
 
 parameters = [thrust, mass, area, dragCoefficient, g, densityAir]; % Parameters needed for velocity calculations
 
 %% Calculations
 
-[t, x] = ode45(@(t,x)velocity(t, x, parameters, launchAngle, burnTime), tspan, x0);
+[t, x] = ode45(@(t, x)velocity(t, x, thrust, mass, area, dragCoefficient, g, densityAir, phi, burnTime, theta), tspan, [x0 xdot0]);
 
 
-function dx = velocity(t, x, parameters, theta, burnTime)
+function dx = velocity(t, x, thrust, mass, area, dragCoefficient, g, densityAir, phi, burnTime, theta)
 
-% Acceleration = (force of thrust - force of gravity - force of drag) / mass
-
-if t < burnTime
 dx = [
-    1;
-    1;
-    (parameters(1) * sin(theta) - parameters(5) * parameters(2) - (parameters(4) * parameters(3) * parameters(6)) * x(3)^2 / 2) / parameters(2)
-    ];
-else
-
-    dx = [
-    1;
-    1;
-    (- parameters(5) * parameters(2) - (parameters(4) * parameters(3) * parameters(6)) * x(3)^2 / 2) / parameters(2)
-    ];
-end
+    x(4); % X component, posiiton
+    x(5); % Y component, position
+    x(6); % Z component, position
+    ((t <= burnTime) * thrust * cos(theta) * sin(phi) - ((dragCoefficient * area * densityAir) * x(6)^2 / 2) * cos(theta) * sin(phi)) / mass; % X component, velocity
+    ((t <= burnTime) * thrust * sin(theta) * sin(phi) - ((dragCoefficient * area * densityAir) * x(6)^2 / 2) * sin(theta) * sin(phi)) / mass; % Y component, velocity
+    ((t <= burnTime) * thrust * cos(phi) - g * mass - ((dragCoefficient * area * densityAir) * x(6)^2 / 2) * cos(phi)) / mass]; % Z component, velocity
 
 end
-
 %% Display
+figure(1)
+title('Rocket Height vs Time')
+plot(t,x(:,3),'k')
+xlabel('Time (s)')
+ylabel('Height (m)')
+grid on
+
+figure(2);
+title('Rocket Velocity vs Time')
+plot(t, x(:,6), 'r')
+xlabel('Time (s)')
+ylabel('Velcoity (m/s)')
+grid on
